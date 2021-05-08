f9109906da45e16b21df9bb06570f4be7b4fe94e
Valentine Ssebuyungo <vssebuyungo@gmail.com>
2021-05-08 10:58:44 -0700

    IOStreams/Manipulations

--- Learning_c++/Exception_handling/main.cpp
+++ Learning_c++/Exception_handling/main.cpp
@@ -2,17 +2,30 @@
 
 using namespace std;
 
+class DivideByZeroException {
+    public:
+        string msg {" Attempted to divide by zero"};
+};
+
+class NegativeException {
+    public:
+        string msg {"Negative value error"};
+};
+
 double calculate_mpg (int miles, int gallons){
     if (gallons == 0){
-        throw 0;
+        throw DivideByZeroException();
     }
     if (gallons < 0 || miles < 0){
-        throw std::string{" Negative value error"};
+        throw NegativeException();
     }
     return static_cast<double>(miles)/gallons;
 }
 
 int main (){
+    std::cout << std::boolalpha;
+    std::cout << " boolalpha (10 == 20)" <<(10 == 20) << endl;
+    std::cout <<  " boolalpha (20 == 20)"<<(20 == 20) << endl;
     int miles, gallons;
     double miles_per_gallon;
 
@@ -25,11 +38,11 @@ int main (){
         miles_per_gallon = calculate_mpg(miles, gallons);
         cout << " Miles per gallon is :" << miles_per_gallon << endl;
     }
-    catch (int &exception){
-        cout << " You cannot divide by zero \n";
+    catch (const DivideByZeroException &ex){
+        cout << ex.msg << endl;
     }
-    catch (std::string &err){
-        cout << err << endl;
+    catch (const NegativeException &err){
+        cout << err.msg << endl;
     }
     catch (...){
         cout << " Unknown exception";
