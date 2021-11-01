6a2cb626fa5c9015152563d24c22367ef7f3a059
Valentine Ssebuyungo <vssebuyungo@gmail.com>
2021-11-01 14:09:55 -0700

    doing challenges

--- Learning_c++/Polymorphism/Interfaces/interfaces.cpp
+++ Learning_c++/Polymorphism/Interfaces/interfaces.cpp
@@ -9,7 +9,7 @@ class I_Printable {
     friend std::ostream &operator<<(std::ostream &os, const I_Printable &obj);
 public:
     virtual void print(std::ostream &os) const = 0;
-    virtual ~I_Printable {};
+    virtual ~I_Printable(){};
 };
 
 std::ostream &operator<<(std::ostream &os, const I_Printable &obj) {
