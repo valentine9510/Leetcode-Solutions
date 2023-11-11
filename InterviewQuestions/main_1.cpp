90667e1664b432d43b0bbcdbcb49078a2efee87e
Valentine Ssebuyungo <vssebuyungo@gmail.com>
2023-11-11 15:41:06 -0800

    Moved and renamed files

--- InterviewQuestions/main_1.cpp
+++ InterviewQuestions/main_1.cpp
@@ -66,6 +66,85 @@ string addBinary(string a, string b) {
         return answer;
     }
 
+/**
+@brief Effecient solution
+We start by initializing two variables: maxSum and currentSum.
+maxSum represents the maximum sum encountered so far and is initially set to the minimum possible integer value 
+to ensure that any valid subarray sum will be greater than it.
+
+currentSum represents the current sum of the subarray being considered and is initially set to 0.
+
+We iterate through the nums array using a for loop, starting from the first element and going up to the last element.
+
+For each element in the array, we add it to the current sum currentSum. This calculates the sum of the subarray ending at the current element.
+
+Next, we check if the current sum currentSum is greater than the current maximum sum maxSum.
+
+If it is, we update maxSum with the new value of currentSum. This means we have found a new maximum subarray sum.
+
+If the current sum currentSum becomes negative, it indicates that including the current element in the subarray would reduce the overall sum. 
+
+In such cases, we reset currentSum to 0. This effectively discards the current subarray and allows us to start a fresh subarray from the next element.
+We repeat steps 3 to 5 for each element in the array.
+
+After iterating through the entire array, the variable maxSum will contain the maximum subarray sum encountered.
+
+Finally, we return the value of maxSum as the result, representing the maximum sum of a contiguous subarray within the given array nums.
+ * 
+ * @param nums 
+ * @return int 
+ */
+int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+
+/**
+ * @brief Start with the largest subarray
+ * Use a sliding window to find the largest sum
+ * This is not effecient
+ * 
+ * @param nums 
+ * @return int 
+ */
+int maxSubArray(vector<int>& nums) {
+    int largestSubArray = INT_MIN;
+    if(nums.size() == 0) return 0;
+
+    for(int sizeOfArray = 1; sizeOfArray<= nums.size(); sizeOfArray++){
+        for(int startIndex = 0; startIndex < nums.size(); startIndex++){
+            int endIndex = startIndex+sizeOfArray - 1; //end index
+
+            if(endIndex >= nums.size()) break; //bound conditions
+
+            //sum up values in my sliding window
+            int tempSum = 0;
+            for(int i = startIndex; i <= endIndex; i++){
+                tempSum+=nums.at(i);
+            }
+
+            largestSubArray = max(largestSubArray, tempSum);
+        }
+    }
+
+    return largestSubArray;
+    
+}
+
 unordered_map<int, int> climbStairs_pathways;
 int climbStairs(int n) {
         if(n <= 2) return n;
---
+++ InterviewQuestions/myPlayground.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#include <cctype>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <math.h>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    /**
+     * @brief 
+     * Create a solution array
+     * Plan, insert all the intervals before
+     * if we are in a merging case, we take the 
+     *          minimum of the newIntervalStart and currentIntervalStart
+     *          max of the newIntervalEnd and endInterValEnd
+     * Add all elements after
+     * 
+     * 
+     * @param intervals 
+     * @param newInterval 
+     * @return vector<vector<int>> 
+     */
+   vector<vector<int>> insertInterval(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> answer;
+        int i = 0;
+
+
+        //case 1, intervals before newInterval
+        while(i<intervals.size()  && intervals.at(i).at(1) < newInterval.at(0)){
+            answer.push_back(intervals.at(i)); //just add it to solution
+            i++;
+        }
+
+        while(i<intervals.size()  &&  newInterval.at(1) >= intervals.at(i).at(0)){ //case 2, merging. NewInterval end >= currentInterval start
+            newInterval.at(0) = min(intervals.at(i).at(0), newInterval.at(0));
+            newInterval.at(1) = max(intervals.at(i).at(1), newInterval.at(1));
+            i++;
+        }
+        answer.push_back(newInterval); //handles empty calse 2
+
+
+        while(i < intervals.size()){
+            answer.push_back(intervals.at(i));
+            i++;
+        }
+
+        return answer;    
+    }
+};
+
+
+int main(){
+
+    std::cout << "Solution " << endl;
+
+    return 0;
+}
